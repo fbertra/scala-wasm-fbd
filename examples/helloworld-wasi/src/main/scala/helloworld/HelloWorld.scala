99cab33444b835df0dea469bf8e4e3c0722385ca
Francois Bertrand <fbertra@gmail.com>
2025-10-06 23:16:02 +0000

    only use wasi ffi (replace scala println by calls to wasi)

--- examples/helloworld-wasi/src/main/scala/helloworld/HelloWorld.scala
+++ examples/helloworld-wasi/src/main/scala/helloworld/HelloWorld.scala
@@ -3,10 +3,18 @@ package helloworld
 import scalajs.component.annotation._
 import scalajs.{component => cm}
 
+import scala.scalajs.wasi.cli.Stdout._
+
 object HelloWorld {
   @ComponentExport("wasi:cli/run@0.2.0", "run")
   def run(): cm.Result[Unit, Unit] = {
-    println("Hello world!")
+    val msg = "Hello world by wasi!"
+    val bytes = msg.getBytes ()
+
+    val out = getStdout()
+
+    out.blockingWriteAndFlush (bytes)
+
     new cm.Ok(())
   }
 }
